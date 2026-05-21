a14b317d68cf01e78b2a2cf9631ec7c26ab629e7
abhishekghosh345 <007abhishekghosh@gmail.com>
2026-05-21 19:39:46 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.use(
 );
 app.use(express.json({ limit: '2mb' }));
 
-app.get('/healthz', (_req, res) => {
+app.get('/healthz', (_req: express.Request, res: express.Response) => {
   res.json({ ok: true });
 });
 
